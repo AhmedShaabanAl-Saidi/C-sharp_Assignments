80c8328f4c7fd94fb06cf757c4b7f24292e7ef3a
AhmedShaabanAl-Saidi <ahmedshaaban123321@gmail.com>
2024-12-22 16:28:23 +0200

    Frist 5 questions

--- Program.cs
+++ Program.cs
@@ -4,7 +4,87 @@
     {
         static void Main(string[] args)
         {
-           Console.WriteLine("Hi ");
+            // 1
+            //Console.WriteLine("Please Enter Your Number Can be divided by 3 and 4 :");
+            //bool flag = int.TryParse(Console.ReadLine(), out int number);
+            // if (!flag)
+            // {
+            //     Console.WriteLine("Please Enter a valid number");
+            //     return;
+            // }
+            //if (number % 3 == 0 && number % 4 == 0)
+            //    Console.WriteLine("Yes");
+            //else
+            //    Console.WriteLine("No");
+
+            // 2
+            //Console.WriteLine("Please Enter Your Number :");
+            //bool flag = int.TryParse(Console.ReadLine(), out int number);
+            //if (!flag)
+            //{
+            //    Console.WriteLine("Please Enter a valid number");
+            //    return;
+            //}
+            //if (number < 0)
+            //    Console.WriteLine("Negative Number");
+            //else if (number > 0 )
+            //    Console.WriteLine("Positive Number");
+            //else
+            //    Console.WriteLine("Zero");
+
+            // 3
+            //int[] numbers = new int[3];
+
+            //for (int i = 0; i < 3;)
+            //{
+            //    Console.WriteLine($"Please Enter Your Number {i + 1} :");
+            //    bool flag = int.TryParse(Console.ReadLine(), out int number);
+            //    if (flag)
+            //    {
+            //        numbers[i] = number;
+            //        i++;
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Please Enter a valid number ");
+            //    }
+            //}
+
+            //Console.WriteLine($"Max Number is => {numbers.Max()}");
+            //Console.WriteLine($"Min Number is => {numbers.Min()}");
+
+            // 4
+            //Console.WriteLine("Please Enter Your Number :");
+            //bool flag = int.TryParse(Console.ReadLine(), out int number);
+            //if (!flag)
+            //{
+            //    Console.WriteLine("Please Enter a valid number");
+            //    return;
+            //}
+            //// check if the number is even or odd
+            //if (number % 2 == 0)
+            //    Console.WriteLine("Even Number");
+            //else
+            //    Console.WriteLine("Odd Number");
+
+            // 5
+            //Console.WriteLine("Please Enter Your Character :");
+            //bool flag =char.TryParse(Console.ReadLine() , out char character);
+            //if (!flag)
+            //{
+            //    Console.WriteLine("Please Enter a valid character");
+            //    return;
+            //}
+
+            //character = char.ToLower(character);
+
+            //if (character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u')
+            //    Console.WriteLine("Vowel");
+            //else
+            //    Console.WriteLine("Consonant");
+
+            // 6
+
         }
     }
 }
