d7258f14d5ebc392439ed958962e1572b0e39ae8
AhmedShaabanAl-Saidi <ahmedshaaban123321@gmail.com>
2024-12-29 22:40:10 +0200

    part 3

---
+++ Enums/SeasOn.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1_C_.Enums
+{
+    internal enum SeasOn
+    {
+        Winter = 1,
+        Spring,
+        Summer,
+        Autumn
+    }
+}
--- Program.cs
+++ Program.cs
@@ -23,13 +23,43 @@ namespace Assignment1_C_
             //Console.WriteLine($"User Name => {user.Name}");
             //Console.WriteLine($"User Permissions => {user.Permissions}");
 
+            /////////////////////////////////////////////////////////////////////////////////////
+
             // Part 2
             // WeekDays
             //string[] weekDays= Enum.GetNames(typeof(WeekDays));
             //foreach (var day in weekDays)
             //    Console.WriteLine(day);
 
+            ////////////////////////////////////////////////////////////////////////////////////
+
             // Part 3
+            // Seas on
+            //Console.WriteLine("Please Enter The Season Name You Want know the Months in it :");
+
+            //object SeasonName;
+            //bool isParsed;
+            //do
+            //{
+            //    isParsed = Enum.TryParse(typeof(SeasOn), Console.ReadLine(), true, out SeasonName);
+            //    if (!isParsed)
+            //        Console.WriteLine("Invalid Season Name, Please Enter a valid Season Name");
+            //}
+            //while (!isParsed);
+
+            //if ((SeasOn)SeasonName == SeasOn.Summer)
+            //    Console.WriteLine("The Range for Seasons is June to August");
+            //else if((SeasOn)SeasonName == SeasOn.Spring)
+            //    Console.WriteLine("The Range for Seasons is March to May");
+            //else if ((SeasOn)SeasonName == SeasOn.Winter)
+            //    Console.WriteLine("The Range for Seasons is December to February");
+            //else if ((SeasOn)SeasonName == SeasOn.Autumn)
+            //    Console.WriteLine("The Range for Seasons is September to November");
+
+            /////////////////////////////////////////////////////////////////////////
+            
+            // Part 4
+
 
         }
     }
