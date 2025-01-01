852bc325c0d4a8a48e63577a73b0ed7359dc6990
AhmedShaabanAl-Saidi <ahmedshaaban123321@gmail.com>
2025-01-01 22:05:09 +0200

    struct "Person"

---
+++ Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1_C_
+{
+    internal struct Person
+    {
+        public string Name { get; set; }
+        public byte Age { get; set; }
+
+        public Person(string name, byte age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-           
+            // 1 - struct "Person" 
+            //Person[] Persons = new Person[3];
+
+            //Persons[0] = new Person("Ahmed",25);
+            //Persons[1] = new Person("Mohmed",30);
+            //Persons[2] = new Person("Ali", 35);
+
+            //Console.WriteLine("Persons:");
+            //foreach(Person p in Persons)
+            //{
+            //    Console.WriteLine($"Name: {p.Name} >>> Age: {p.Age}");
+            //}
+
+
         }
     }
 }
