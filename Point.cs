de3af117672afeb148bc014caf165fcce1fdeba2
AhmedShaabanAl-Saidi <ahmedshaaban123321@gmail.com>
2025-01-01 22:40:18 +0200

    distance between two points in a 2D space.

---
+++ Point.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1_C_
+{
+    internal struct Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public Point(double _x, double _y)
+        {
+            X = _x;
+            Y = _y;
+        }
+
+        public double Distance(Point p)
+        {
+            return Math.Sqrt(Math.Pow(X - p.X, 2) + Math.Pow(Y - p.Y, 2));
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,6 +2,24 @@
 {
     internal class Program
     {
+        public static double TryToParse(string point)
+        {
+            double result;
+            bool isParsed;
+            do
+            {
+                isParsed = double.TryParse(Console.ReadLine(), out result);
+                if (!isParsed)
+                {
+                    Console.WriteLine("Invalid input, please enter a number");
+                    Console.Write(point);
+                }
+            }
+            while (!isParsed);
+
+            return result;
+        }
+
         static void Main(string[] args)
         {
             // 1 - struct "Person" 
@@ -17,7 +35,25 @@
             //    Console.WriteLine($"Name: {p.Name} >>> Age: {p.Age}");
             //}
 
+            // 2 - Point
+            //Console.WriteLine("Enter the coordinates of the first point (X1, Y1):");
+            //Console.Write("X1: ");
+            //double x1 = TryToParse("X1: ");
+            //Console.Write("Y1: ");
+            //double y1 = TryToParse("Y1: ");
 
+            //Console.WriteLine("Enter the coordinates of the second point (X2, Y2):");
+            //Console.Write("X2: ");
+            //double x2 = TryToParse("X2: ");
+            //Console.Write("Y2: ");
+            //double y2 = TryToParse("Y2: ");
+
+            //Point p1 = new Point(x1, y1);
+            //Point p2 = new Point(x2, y2);
+
+            //Console.WriteLine($"Distance between p1 and p2: {p1.Distance(p2)}");
+
+            // 3 - 
         }
     }
 }
