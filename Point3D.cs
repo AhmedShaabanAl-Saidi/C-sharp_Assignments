a4c6a823e93db0fe3fd5566180a2e5cbc2c89f3f
AhmedShaabanAl-Saidi <ahmedshaaban123321@gmail.com>
2025-01-22 17:50:19 +0200

    First Project

---
+++ Point3D.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1_C_
+{
+    public class Point3D : IComparable , ICloneable
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+
+        public Point3D(int x, int y) : this(x, y, 0) { }
+
+        public Point3D(int x, int y, int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+        public override string ToString()
+        {
+            return $"Point Coordinates: ({X}, {Y}, {Z})";
+        }
+
+        public int CompareTo(object? obj)
+        {
+            Point3D point = (Point3D)obj;
+
+            int xComparison = X.CompareTo(point.X);
+            if (xComparison != 0)
+                return xComparison;
+
+            return Y.CompareTo(point.Y);
+        }
+
+        public Point3D(Point3D point)
+        {
+            X = point.X;
+            Y = point.Y;
+            Z = point.Z;
+        }
+
+        public object Clone()
+        {
+            return new Point3D(this);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,9 +2,92 @@
 {
     internal class Program
     {
+        #region TryToParse
+        public static int TryToParse(string point)
+        {
+            Console.Write(point);
+
+            int result;
+            bool isParsed;
+            do
+            {
+                isParsed = int.TryParse(Console.ReadLine(), out result);
+                if (!isParsed)
+                {
+                    Console.WriteLine("Invalid input, please enter a number");
+                    Console.Write(point);
+                }
+            }
+            while (!isParsed);
+
+            return result;
+        } 
+        #endregion
+
         static void Main(string[] args)
         {
-           
+            #region First Project
+            // 1,2
+            //Point3D P = new Point3D(10, 10, 10);
+            //Console.WriteLine(P.ToString());
+
+            // 3
+            //Console.WriteLine("Please Enter Coordinates for 2 points P1, P2 :");
+            //int p1 = TryToParse("P1: ");
+            //int p2 = TryToParse("P2: ");
+
+            //Point3D P = new Point3D(p1, p2);
+
+            // 4
+            //if (p1 == p2)
+            //    Console.WriteLine("P1 and P2 are the same point");
+            //else
+            //{
+            //    Console.WriteLine("P1 and P2 are different points");
+            //}
+
+            // 5
+            //Point3D[] points =
+            //{
+            //new Point3D(5, 2),
+            //new Point3D(3, 3),
+            //new Point3D(1, 4),
+            //new Point3D(5, 1),
+            //new Point3D(2, 2)
+            //};
+
+            //Console.WriteLine("Before Sorting");
+            //foreach (Point3D point in points)
+            //{
+            //    Console.WriteLine(point);
+            //}
+
+            //Console.WriteLine("=================================");
+
+            //Array.Sort(points);
+
+            //Console.WriteLine("After Sorting");
+            //foreach (Point3D point in points)
+            //{
+            //    Console.WriteLine(point);
+            //}
+
+            // 6
+            //Point3D p1 = new Point3D(1, 2);
+            //Point3D p2 = new Point3D(10, 20);
+
+            //Console.WriteLine("Before Copy");
+            //Console.WriteLine(p2.GetHashCode());
+            //Console.WriteLine(p2.X);
+
+            //p2 = (Point3D)p1.Clone();
+
+            //Console.WriteLine("=================================");
+            //Console.WriteLine("After Copy");
+            //Console.WriteLine(p2.GetHashCode());
+            //Console.WriteLine(p2.X);
+            #endregion
+
         }
     }
 }
