e59715acb7fda44ca6c3f7aaf4ef7ba5331b97c6
AhmedShaabanAl-Saidi <ahmedshaaban123321@gmail.com>
2025-01-27 00:09:35 +0200

    Assignment OOP  6

---
+++ Duration.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1_C_
+{
+    public class Duration
+    {
+        // 1 - Define Class 
+        public int Hours;
+        public int Minutes;
+        public int Seconds;
+
+        // 2 - Override All System.Object Members
+        public override string ToString()
+        {
+            if (Hours == 0)
+                return $"{Minutes} Minutes, {Seconds} Seconds";
+            else
+                return $"{Hours} Hours, {Minutes} Minutes, {Seconds} Seconds";
+        }
+
+        public override bool Equals(object? obj)
+        {
+            Duration duration = (Duration)obj;
+
+            return (Hours == duration.Hours) && (Minutes == duration.Minutes) && (Seconds == duration.Seconds);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Hours, Minutes, Seconds);
+        }
+
+        // 3 - Define All Required Constructors
+        public Duration(int hours, int minutes, int seconds)
+        {
+            Hours = hours;
+            Minutes = minutes;
+            Seconds = seconds;
+        }
+
+        public Duration(int seconds)
+        {
+            Hours = seconds / 3600;
+            Minutes = (seconds % 3600) / 60;
+            Seconds = (seconds % 3600) % 60;
+        }
+
+        // 4 - Implement All required Operators overloading
+        // D3=D1+D2
+        public static Duration operator +(Duration d1,Duration d2)
+        {
+            return new Duration((d1?.Hours ?? 0) + (d2?.Hours ?? 0), (d1?.Minutes ?? 0) + (d2?.Minutes ?? 0), (d1?.Seconds ?? 0) + (d2?.Seconds ?? 0));
+        }
+
+        // D3=D1+ 7800 , D3=D3+666
+        public static Duration operator +(Duration d1, int seconds)
+        {
+            return new Duration((d1?.Hours ?? 0) + (seconds / 3600), (d1?.Minutes ?? 0) + ((seconds % 3600) / 60), (d1?.Seconds ?? 0) + ((seconds % 3600) % 60));
+        }
+
+        // D3= ++D1 (Increase One Minute)
+        public static Duration operator ++(Duration duration)
+        {
+            return new Duration(duration.Hours, duration.Minutes + 1, duration.Seconds);
+        }
+
+        // D3= --D2 (Decrease One Minute)
+        public static Duration operator --(Duration duration)
+        {
+            return new Duration(duration.Hours, duration.Minutes - 1, duration.Seconds);
+        }
+
+        // D1=D1-D2
+        public static Duration operator -(Duration d1, Duration d2)
+        {
+            return new Duration((d1?.Hours ?? 0) - (d2?.Hours ?? 0), (d1?.Minutes ?? 0) - (d2?.Minutes ?? 0), (d1?.Seconds ?? 0) - (d2?.Seconds ?? 0));
+        }
+
+        // If (D1>D2)
+        public static bool operator >(Duration d1, Duration d2)
+        {
+            return (d1?.Hours ?? 0) > (d2?.Hours ?? 0) && (d1?.Minutes ?? 0) > (d2?.Minutes ?? 0) && (d1?.Seconds ?? 0) > (d2?.Seconds ?? 0);
+        }
+
+        public static bool operator <(Duration d1, Duration d2)
+        {
+            return (d1?.Hours ?? 0) < (d2?.Hours ?? 0) && (d1?.Minutes ?? 0) < (d2?.Minutes ?? 0) && (d1?.Seconds ?? 0) < (d2?.Seconds ?? 0);
+        }
+
+        // If (D1<=D2)
+        public static bool operator >=(Duration d1, Duration d2)
+        {
+            return (d1?.Hours ?? 0) >= (d2?.Hours ?? 0) && (d1?.Minutes ?? 0) >= (d2?.Minutes ?? 0) && (d1?.Seconds ?? 0) >= (d2?.Seconds ?? 0);
+        }
+
+        public static bool operator <=(Duration d1, Duration d2)
+        {
+            return (d1?.Hours ?? 0) <= (d2?.Hours ?? 0) && (d1?.Minutes ?? 0) <= (d2?.Minutes ?? 0) && (d1?.Seconds ?? 0) <= (d2?.Seconds ?? 0);
+        }
+
+        // If (D1)
+        public static bool operator true(Duration duration)
+        {
+            return (duration?.Hours ?? 0) > 0 || (duration?.Minutes ?? 0) > 0 || (duration?.Seconds ?? 0) > 0;
+        }
+
+        public static bool operator false(Duration duration)
+        {
+            return (duration?.Hours ?? 0) <= 0 && (duration?.Minutes ?? 0) <= 0 && (duration?.Seconds ?? 0) <= 0;
+        }
+
+        // DateTime Obj = (DateTime) D1
+        public static explicit operator DateTime(Duration duration)
+        {
+            return new DateTime(1, 1, 1, duration.Hours, duration.Minutes, duration.Seconds);
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,56 @@
     {
         static void Main(string[] args)
         {
-           
+            // 3
+
+            //Duration d1 = new Duration(1, 10, 15);
+            //Console.WriteLine(d1);
+
+            Duration d1 = new Duration(3600);
+            //Console.WriteLine(d1);
+
+            //Duration d2 = new Duration(7800);
+            //Console.WriteLine(d2);
+
+            //Duration d3 = new Duration(666);
+            //Console.WriteLine(d3);
+
+            // 4
+            //d3 = d1 + d2;
+            //Console.WriteLine($"d1 + d2 => {d3}");
+
+            //d3 = d1 + 7800;
+            //Console.WriteLine($"d1 + 7800 => {d1 + 7800}");
+
+            //d3 = d3 + 666;
+            //Console.WriteLine($"d3 + 666 => {d3}");
+
+            //d3 = ++d1;
+            //Console.WriteLine($"++d1 => {d3}");
+
+            //d3 = --d1;
+            //Console.WriteLine($"--d1 => {d3}");
+
+            //d1 = d1 - d2;
+            //Console.WriteLine($"d1 - d2 => {d1}");
+
+            //if (d1 > d2)
+            //    Console.WriteLine("d1 > d2");
+            //else
+            //    Console.WriteLine("d1 < d2");
+
+            //if (d1 <= d2)
+            //    Console.WriteLine("d1 <= d2");
+            //else
+            //    Console.WriteLine("d1 > d2");
+
+            //if (d1)
+            //    Console.WriteLine("d1");
+            //else
+            //    Console.WriteLine("Not d1");
+
+            //DateTime Obj = (DateTime)d1;
+            //Console.WriteLine(Obj.ToString("HH:mm:ss")); 
         }
     }
 }
