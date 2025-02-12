d69b5e38bb3f7c3d9227b80625f364a1d609e24c
AhmedShaabanAl-Saidi <ahmedshaaban123321@gmail.com>
2025-02-12 21:54:50 +0200

    Club

---
+++ Clubs/Club.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assignment1_C_.Employees;
+
+namespace Assignment1_C_.Clubs
+{
+    public class Club
+    {
+        public int ClubID { get; set; }
+        public String ClubName { get; set; }
+        List<Employee> Members = new List<Employee>();
+        public void AddMember(Employee employee)
+        {
+            ///Try Register for EmployeeLayOff Event Here
+            employee.OnEmployeeLayOff += RemoveMember;
+            Members.Add(employee);
+        }
+        ///CallBackMethod
+        public void RemoveMember(object sender, EmployeeLayOffEventArgs employeeLayOffEvent)
+        {
+            ///Employee will be removed from Club if Vacation Stock < 0
+            if (sender is Employee employee)
+            {
+                if (employeeLayOffEvent.Cause == LayOffCause.VacationStock)
+                {
+                    Members.Remove(employee);
+                    Console.WriteLine($"Employee {employee.EmployeeID} removed from Club {ClubName} because of : {employeeLayOffEvent.Cause}");
+                }
+                else
+                {
+                    ///Employee Will not be removed from the Club if Age>60
+                    Console.WriteLine($"Employee {employee.EmployeeID} Will not be removed {ClubName}");
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using Assignment1_C_.Departments;
+﻿using Assignment1_C_.Clubs;
+using Assignment1_C_.Departments;
 using Assignment1_C_.Employees;
 
 namespace Assignment1_C_
@@ -34,6 +35,17 @@ namespace Assignment1_C_
             //employee2.VacationStock = -5;
             //employee3.BirthDate = new DateTime(1955,1,22);
 
+            //Club
+            //Club companyClub = new Club { ClubID = 1, ClubName = "Company Club" };
+
+            //Employee employee1 = new Employee { EmployeeID = 101, BirthDate = new DateTime(1955, 1, 1), VacationStock = 5 };
+            //Employee employee2 = new Employee { EmployeeID = 102, BirthDate = new DateTime(1990, 1, 1), VacationStock = -2 };
+
+            //companyClub.AddMember(employee1);
+            //companyClub.AddMember(employee2);
+
+            //employee1.BirthDate = new DateTime(1960, 1, 1); 
+            //employee2.VacationStock = -5; 
 
 
         }
