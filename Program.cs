19173a12588266b6adf3bc47d988924ff53b425e
AhmedShaabanAl-Saidi <ahmedshaaban123321@gmail.com>
2025-02-17 11:41:43 +0200

    Sales EmployeLINQ - Restriction Operatorse & Board Member

--- Program.cs
+++ Program.cs
@@ -1,10 +1,33 @@
-﻿namespace Assignment1_C_
+﻿using static Assignment1_C_.ListGenerator;
+
+namespace Assignment1_C_
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-           
+            #region LINQ - Restriction Operators
+            // LINQ - Restriction Operators
+            // 1. Find all products that are out of stock.
+            //var result = ProductList.Where(product => product.UnitsInStock == 0);
+
+            //foreach (var product in result)
+            //    Console.WriteLine(product);
+
+            // 2. Find all products that are in stock and cost more than 3.00 per unit.
+            //var result = ProductList.Where(product => product.UnitsInStock != 0 && product.UnitPrice > 3.00m);
+
+            //foreach (var product in result)
+            //    Console.WriteLine(product);
+
+            // 3. Returns digits whose name is shorter than their value.
+            //string[] Arr = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+            //var result = Arr.Where((name, index) => name.Length < index);
+
+            //foreach (var product in result)
+            //    Console.WriteLine(product); 
+            #endregion
         }
     }
 }
