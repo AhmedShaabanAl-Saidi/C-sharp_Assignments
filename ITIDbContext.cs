7bc7abe66aa45bcaa4d2aee00fe8037983320e73
AhmedShaabanAl-Saidi <ahmedshaaban123321@gmail.com>
2025-03-01 09:01:06 +0200

    ..

---
+++ ITIDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Assignment1_C_
+{
+    public class ITIDbContext : DbContext
+    {
+        //public DbSet<>
+
+        // Connection String
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=AHMEDSHAABAN;database=CompanyDb;trusted_connection=true;trustServerCertificate=true").UseQueryTrackingBehavior(QueryTrackingBehavior.TrackAll);
+        }
+    }
+}
---
+++ Models/Instructor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1_C_.Models
+{
+    public class Instructor
+    {
+        public int InstructorId { get; set; }
+        public string? Name { get; set; } // Nullable
+        public decimal Salary { get; set; }
+        public decimal Bonus { get; set; }
+        public string Address { get; set; } // Not Accept Null
+        public decimal HourlyRate { get; set; }
+        public int DepartmentId { get; set; }
+    }
+}
